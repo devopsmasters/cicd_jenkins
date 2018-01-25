79ba08d847f1bf80aec93e6c4c93c8744c84efc6
Rajesh Gopal <rajeshsmyl@gmail.com>
2018-01-26 01:12:52 +0530

    rename module

--- manifests/master.pp
+++ manifests/master.pp
@@ -2,7 +2,7 @@
 
 include jenkins
 
-include master
+include masterconfig
 
 exec { 'waitonmaster':
   command => 'sleep 60',
@@ -12,17 +12,17 @@ exec { 'waitonmaster':
 exec { 'addjob1':
   command => "curl -s -XPOST 'http://localhost:8080/createItem?name=sample-maven-artifactory-job' --data-binary @maven-sample-job.xml -H 'Content-Type:text/xml' ",
   path    => ['/usr/bin', '/bin'],
-  cwd     => '/opt/garage/modules/master/files',
+  cwd     => '/opt/garage/modules/masterconfig/files',
 }
 exec { 'addjob2':
   command => "curl -s -XPOST 'http://localhost:8080/createItem?name=ci-build-pipeline-demo' --data-binary @ci-build-pipeline.xml -H 'Content-Type:text/xml' ",
   path    => ['/usr/bin', '/bin'],
-  cwd     => '/opt/garage/modules/master/files',
+  cwd     => '/opt/garage/modules/masterconfig/files',
 }
 exec { 'addjob3':
   command => "curl -s -XPOST 'http://localhost:8080/createItem?name=cd-pipeline-demo' --data-binary @cd-pipeline-demo.xml -H 'Content-Type:text/xml' ",
   path    => ['/usr/bin', '/bin'],
-  cwd     => '/opt/garage/modules/master/files',
+  cwd     => '/opt/garage/modules/masterconfig/files',
 }
 
-Class['jenkins'] -> Exec['waitonmaster'] -> Exec['addjob1'] -> Exec['addjob2'] -> Exec['addjob3'] -> Class['master']
+Class['jenkins'] -> Exec['waitonmaster'] -> Exec['addjob1'] -> Exec['addjob2'] -> Exec['addjob3'] -> Class['masterconfig']
