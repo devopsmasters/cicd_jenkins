ac7096c1f33bf1e6c327513ab9dcc375148d1906
rajeshgopal <rajeshsmyl@gmail.com>
2018-01-26 01:08:41 +0530

    Update master.pp

--- manifests/master.pp
+++ manifests/master.pp
@@ -15,9 +15,14 @@ exec { 'addjob1':
   cwd     => '/opt/garage/modules/master/files',
 }
 exec { 'addjob2':
-  command => "curl -s -XPOST 'http://localhost:8080/createItem?name=docker-build-pipeline-job' --data-binary @docker-build-pipeline.xml -H 'Content-Type:text/xml' ",
+  command => "curl -s -XPOST 'http://localhost:8080/createItem?name=ci-build-pipeline-demo' --data-binary @ci-build-pipeline.xml -H 'Content-Type:text/xml' ",
+  path    => ['/usr/bin', '/bin'],
+  cwd     => '/opt/garage/modules/master/files',
+}
+exec { 'addjob3':
+  command => "curl -s -XPOST 'http://localhost:8080/createItem?name=cd-pipeline-demo' --data-binary @cd-pipeline-demo.xml -H 'Content-Type:text/xml' ",
   path    => ['/usr/bin', '/bin'],
   cwd     => '/opt/garage/modules/master/files',
 }
 
-Class['jenkins'] -> Exec['waitonmaster'] -> Exec['addjob1'] -> Exec['addjob2'] -> Class['master']
+Class['jenkins'] -> Exec['waitonmaster'] -> Exec['addjob1'] -> Exec['addjob2'] -> Exec['addjob3'] -> Class['master']
